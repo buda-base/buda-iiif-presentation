55a18097f46a6b0fea4e49b651183149a304258e
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-03-28 13:15:01 +0200

    test for JENA-1513

--- src/test/java/io/bdrc/iiif/presentation/PresentationTest.java
+++ src/test/java/io/bdrc/iiif/presentation/PresentationTest.java
@@ -1,5 +1,9 @@
 package io.bdrc.iiif.presentation;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
 import org.apache.http.HttpResponse;
@@ -8,9 +12,21 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFactory;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.ResultSetMgr;
+import org.apache.jena.riot.resultset.ResultSetLang;
+import org.apache.jena.sparql.engine.QueryExecutionBase;
 import org.junit.Test;
+
+import com.amazonaws.util.IOUtils;
 
 public class PresentationTest {
 
@@ -71,28 +87,66 @@ public class PresentationTest {
 //        }
 //    }
 
-    @Test
-    public void testLds() {
-        HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead 
-        try {
-            HttpPost request = new HttpPost("http://purl.bdrc.io/graph/IIIFPres_workGraph");
-            StringEntity params = new StringEntity("{\"R_RES\":\"bdr:W22084\"}", ContentType.APPLICATION_JSON);
-            request.addHeader(HttpHeaders.ACCEPT, "text/turtle");
-            request.setEntity(params);
-            HttpResponse response = httpClient.execute(request);
-            int code = response.getStatusLine().getStatusCode();
-            if (code != 200) {
-                System.out.println("server error!");
-                // TODO: do something
-                return;
-            }
-            HttpEntity resEntity = response.getEntity();
-            Model m = ModelFactory.createDefaultModel();
-            m.read(resEntity.getContent(), null, "TURTLE");
-            System.out.println(m.toString());
-        }catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
+//    @Test
+//    public void testLds() {
+//        HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead 
+//        try {
+//            HttpPost request = new HttpPost("http://purl.bdrc.io/graph/IIIFPres_workGraph");
+//            StringEntity params = new StringEntity("{\"R_RES\":\"bdr:W22084\"}", ContentType.APPLICATION_JSON);
+//            request.addHeader(HttpHeaders.ACCEPT, "text/turtle");
+//            request.setEntity(params);
+//            HttpResponse response = httpClient.execute(request);
+//            int code = response.getStatusLine().getStatusCode();
+//            if (code != 200) {
+//                System.out.println("server error!");
+//                // TODO: do something
+//                return;
+//            }
+//            HttpEntity resEntity = response.getEntity();
+//            Model m = ModelFactory.createDefaultModel();
+//            m.read(resEntity.getContent(), null, "TURTLE");
+//            System.out.println(m.toString());
+//        }catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//    }
     
+//    @Test
+//    public void testLdsJson() {
+//        HttpClient httpClient = HttpClientBuilder.create().build(); //Use this instead 
+//        try {
+//            HttpPost request = new HttpPost("http://purl.bdrc.io/query/IIIFPres_itemInfo");
+//            StringEntity params = new StringEntity("{\"R_RES\":\"bdr:I29329\"}", ContentType.APPLICATION_JSON);
+//            //request.addHeader(HttpHeaders.ACCEPT, "application/json");
+//            request.setEntity(params);
+//            HttpResponse response = httpClient.execute(request);
+//            int code = response.getStatusLine().getStatusCode();
+//            if (code != 200) {
+//                System.out.println("server error!");
+//                // TODO: do something
+//                return;
+//            }
+//            HttpEntity resEntity = response.getEntity();
+//            InputStream body = resEntity.getContent();
+//            ResultSetLang.init();
+//            ResultSet res = ResultSetMgr.read(body, ResultSetLang.SPARQLResultSetJSON);
+//        } catch (Exception ex) {
+//            ex.printStackTrace();
+//        }
+//    }
+    
+    @Test
+    public void testInit() {
+        String sparqljson = "{\n" + 
+                "  \"head\" : {\n" + 
+                "    \"vars\" : [ \"work\" ]\n" + 
+                "  },\n" + 
+                "  \"results\" : {\n" + 
+                "    \"bindings\" : [ ]\n" + 
+                "  }\n" + 
+                "}";
+        InputStream stream = new ByteArrayInputStream(sparqljson.getBytes(StandardCharsets.UTF_8));
+        //ResultSetLang.init();
+        ResultSet res = ResultSetMgr.read(stream, ResultSetLang.SPARQLResultSetJSON);
+    }
 }
