24b2a1d0566afe1aa77dce3807668a939032f20e
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-08-06 16:02:56 +0200

    another attempt to fix cors

--- src/main/java/io/bdrc/iiif/presentation/CommonHeadersFilter.java
+++ src/main/java/io/bdrc/iiif/presentation/CommonHeadersFilter.java
@@ -14,14 +14,16 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class CommonHeadersFilter implements ContainerResponseFilter {
 
+    static final int ACCESS_CONTROL_MAX_AGE_IN_SECONDS = 24 * 60 * 60;
+    
     @Override
     public void filter(final ContainerRequestContext request, final ContainerResponseContext response) throws IOException {
         final MultivaluedMap<String, Object> headers = response.getHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
         headers.add("Access-Control-Allow-Credentials", "true");
         headers.add("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS");
-        headers.add("Access-Control-Allow-Headers", "Origin, Authorization, Keep-Alive, User-Agent, If-Modified-Since, If-None-Match, Cache-Control");
-        final int ACCESS_CONTROL_MAX_AGE_IN_SECONDS = 24 * 60 * 60;
+        headers.add("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, Keep-Alive, User-Agent, If-Modified-Since, If-None-Match, Cache-Control");
+        headers.add("Access-Control-Expose-Headers", "Cache-Control, ETag, Last-Modified, Content-Type, Cache-Control, Vary, Access-Control-Max-Age");
         headers.add("Access-Control-Max-Age", ACCESS_CONTROL_MAX_AGE_IN_SECONDS);
     }
 }
