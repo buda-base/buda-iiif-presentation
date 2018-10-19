df4533c43aa48565e11a422263fad2eb31e08faa
MarcAgate <agate.marc@gmail.com>
2018-10-19 14:34:30 +0200

    Upgraded to bdrc-auth-lib 0.3.0

--- src/main/java/io/bdrc/iiif/presentation/IIIFPresApplication.java
+++ src/main/java/io/bdrc/iiif/presentation/IIIFPresApplication.java
@@ -5,14 +5,18 @@ import javax.ws.rs.ext.Provider;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import io.bdrc.auth.AuthProps;
 import io.bdrc.auth.rdf.RdfAuthModel;
 
 @ApplicationPath("/")
 @Provider
 public class IIIFPresApplication extends ResourceConfig {
 
+    public final static String AUTH_PROPS_FILE="/etc/buda/iiifpres/iiifpres-auth.properties";
+
     public IIIFPresApplication() {
-        //RdfAuthModel.init();
+        AuthProps.init(AUTH_PROPS_FILE);
+        RdfAuthModel.init();
         register(CommonHeadersFilter.class);
         register(IIIFPresAuthFilter.class);
     }
