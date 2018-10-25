c4b31340912c38f8b02863f5dbf65a2df3ec77ba
MarcAgate <agate.marc@gmail.com>
2018-10-25 20:50:35 +0200

    Upgraded to bdrc-auth-lib 0.4.0

--- src/main/java/io/bdrc/iiif/presentation/IIIFPresApplication.java
+++ src/main/java/io/bdrc/iiif/presentation/IIIFPresApplication.java
@@ -1,9 +1,16 @@
 package io.bdrc.iiif.presentation;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.ext.Provider;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.bdrc.auth.AuthProps;
 import io.bdrc.auth.rdf.RdfAuthModel;
@@ -12,14 +19,25 @@ import io.bdrc.auth.rdf.RdfAuthModel;
 @Provider
 public class IIIFPresApplication extends ResourceConfig {
 
-    public final static String AUTH_PROPS_FILE="/etc/buda/iiifpres/iiifpres-auth.properties";
+    static final String configPath= System.getProperty("iiifpres.configpath");
+    public final static Logger log=LoggerFactory.getLogger(IIIFPresApplication.class.getName());
 
     public IIIFPresApplication() {
-        AuthProps.init(AUTH_PROPS_FILE);
-        RdfAuthModel.init();
-        ServiceCache.init();
-        register(CommonHeadersFilter.class);
-        register(IIIFPresAuthFilter.class);
+        InputStream is;
+        try {
+            is = new FileInputStream(configPath+"iiifpres-auth.properties");
+            Properties props=new Properties();
+            props.load(is);
+            AuthProps.init(props);
+            RdfAuthModel.init();
+            ServiceCache.init();
+            register(CommonHeadersFilter.class);
+            register(IIIFPresAuthFilter.class);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
 
 }
