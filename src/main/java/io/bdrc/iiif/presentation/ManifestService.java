61fb44cd6be86f650ebb5823aa1ab9ffc5697fa4
MarcAgate <agate.marc@gmail.com>
2018-11-08 15:29:06 +0100

    Applied viewHints to manifest instead of sequence

--- src/main/java/io/bdrc/iiif/presentation/ManifestService.java
+++ src/main/java/io/bdrc/iiif/presentation/ManifestService.java
@@ -114,10 +114,12 @@ public class ManifestService {
         manifest.addLogo("https://eroux.fr/logo.png");
         manifest.addLabel(id.getVolumeId());
         final Sequence mainSeq = getSequenceFrom(id, imageInfoList);
-        mainSeq.setViewingDirection(ViewingDirection.TOP_TO_BOTTOM);
+        //mainSeq.setViewingDirection(ViewingDirection.TOP_TO_BOTTOM);
         /***Viewing hints and direction*/
-        mainSeq.setViewingHints(VIEW_HINTS);
+        //mainSeq.setViewingHints(VIEW_HINTS);
         manifest.addSequence(mainSeq);
+        manifest.setViewingHints(VIEW_HINTS);
+        manifest.setViewingDirection(ViewingDirection.TOP_TO_BOTTOM);
         return manifest;
     }
 
