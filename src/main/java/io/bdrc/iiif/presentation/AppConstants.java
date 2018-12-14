8dcf0c710e9c737199370df096fe6d1745c67119
MarcAgate <agate.marc@gmail.com>
2018-12-14 12:00:10 +0100

    changed IIIF_IMAGE_PREFIX

--- src/main/java/io/bdrc/iiif/presentation/AppConstants.java
+++ src/main/java/io/bdrc/iiif/presentation/AppConstants.java
@@ -8,27 +8,28 @@ public class AppConstants {
     public static final String TMPPREFIX  = "http://purl.bdrc.io/ontology/tmp/";
     public static final String IIIFPresPrefix = "http://presentation.bdrc.io/2.1.1/";
     public static final String IIIFPresPrefix_coll = IIIFPresPrefix+"collection/";
-    
+
     public static final String LDS_QUERYPREFIX = "http://buda1.bdrc.io/";
     public static final String LDS_WORKGRAPH_QUERY = LDS_QUERYPREFIX+"graph/IIIFPres_workGraph_noItem";
     public static final String LDS_ITEMGRAPH_QUERY = LDS_QUERYPREFIX+"graph/IIIFPres_itemGraph";
     public static final String LDS_VOLUME_QUERY = LDS_QUERYPREFIX+"query/IIIFPres_volumeInfo";
-    
-    
-    public static final String IIIF_IMAGE_PREFIX = "http://iiif.bdrc.io/image/v2/";
-    
+
+
+    //public static final String IIIF_IMAGE_PREFIX = "http://iiif.bdrc.io/image/v2/";
+    public static final String IIIF_IMAGE_PREFIX = "http://iiif.bdrc.io/";
+
 	public static final int GENERIC_APP_ERROR_CODE = 5001;
-	
+
 	public static final int INVALID_IDENTIFIER_ERROR_CODE = 5002;
-	
+
 	public static final int NO_ACCESS_ERROR_CODE = 5003;
-	
+
 	public static final int FILE_LIST_ACCESS_ERROR_CODE = 5004;
-	
+
 	public static final int CANNOT_FIND_VOLUME_ERROR_CODE = 5005;
 	public static final int CANNOT_FIND_ITEM_ERROR_CODE = 5006;
 	public static final int CANNOT_FIND_WORK_ERROR_CODE = 5007;
-	
+
 	public static final int GENERIC_LDS_ERROR = 5008;
 	public static final int GENERIC_CACHE_ERROR = 5009;
 
