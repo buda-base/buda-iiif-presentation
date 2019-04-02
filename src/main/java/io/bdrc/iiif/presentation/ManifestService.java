e6654eeb57edde1ddfc1559af247b30c37d324d0
Elie Roux <elie.roux@telecom-bretagne.eu>
2019-04-02 17:02:31 +0200

    allow wv: (oops)

--- src/main/java/io/bdrc/iiif/presentation/ManifestService.java
+++ src/main/java/io/bdrc/iiif/presentation/ManifestService.java
@@ -155,6 +155,7 @@ public class ManifestService {
     public static Manifest getManifestForIdentifier(final Identifier id, final VolumeInfo vi, boolean continuous, final WorkInfo wi) throws BDRCAPIException {
         if (id.getType() != Identifier.MANIFEST_ID || (
                 id.getSubType() != Identifier.MANIFEST_ID_VOLUMEID
+                && id.getSubType() != Identifier.MANIFEST_ID_WORK_IN_VOLUMEID
                 && id.getSubType() != Identifier.MANIFEST_ID_VOLUMEID_OUTLINE)) {
             throw new BDRCAPIException(404, GENERIC_APP_ERROR_CODE, "you cannot access this type of manifest yet");
         }
