2ac86ad6a6c9421b5da74bfb938dbc7bd7b8fe81
eroux <elie.roux@telecom-bretagne.eu>
2020-11-14 09:47:48 +0100

    simplify geoloc cache

--- src/main/java/io/bdrc/iiif/presentation/GeoLocation.java
+++ src/main/java/io/bdrc/iiif/presentation/GeoLocation.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.maxmind.db.CHMCache;
 import com.maxmind.geoip2.DatabaseReader;
 import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.maxmind.geoip2.model.CountryResponse;
@@ -27,7 +28,7 @@ public class GeoLocation {
 	public static DatabaseReader getDbReader() {
 		try {
 			File database = new File(DBLocation);
-			return new DatabaseReader.Builder(database).withCache(new JCSNodeCache()).build();
+			return new DatabaseReader.Builder(database).withCache(new CHMCache()).build();
 		} catch (IOException e) {
 			log.error("getDbReader()", e);
 			return null;
